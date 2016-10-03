671e71952f854b6d3d931bb60c0c5482243dd1c4
David Kananda <kd43@gre.ac.uk>
2016-10-03 23:28:31 +0100

    minor updates

--- src/app/scripts/components/app.module.ts
+++ src/app/scripts/components/app.module.ts
@@ -1,27 +1,14 @@
-import { NgModule, provide } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { LocationStrategy, HashLocationStrategy } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { HttpModule } from "@angular/http";
 
-import { AppComponent, COMPONENT_DECLARATIONS } from "./";
-import { ROUTING } from "./app.routing";
+import { AppComponent } from "./app.component";
 
-import { APP_PROVIDERS } from "./app-providers";
-import { appState } from "./app-state/app-state";
-import { APP_STATE_ACTIONS } from "./app-state/app-state-actions";
 
 @NgModule({
     declarations: [
-        ...COMPONENT_DECLARATIONS
+        
     ],
-    imports: [ BrowserModule, ROUTING, FormsModule, ReactiveFormsModule, HttpModule ],
-    bootstrap: [ AppComponent ],
-    providers: [ APP_PROVIDERS,
-                provide(LocationStrategy, { useClass: HashLocationStrategy }),
-                provide('AppState', { useValue: appState }),
-                APP_STATE_ACTIONS 
-    ]
+    bootstrap: [ AppComponent ]
 })
 export class AppModule {
 
